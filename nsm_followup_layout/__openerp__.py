8ec70056507a9d948f072111e990d437b7284cb9
Erwin van der Ploeg <erwin@bas-solutions.nl>
2014-10-29 12:31:47 +0100

    Module follow-up

---
+++ nsm_followup_layout/__openerp__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 BAS Solutions
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "NSM Payment Followup Layout",
+    "version": "1.0",
+    "author": "BAS Solutions",
+    "website": "http://www.bas-solutions.nl",
+    "category": "Sales Management",
+    "depends": ["base", "account_followup","bas_erp_company_header"],
+    "summary": "Customized Payment Followup layout",
+    "description": """
+                This is the internal Customized Payment Followup layout
+     """,
+    'images': [
+               ],
+    "init_xml": [
+                 ],
+    "update_xml": [
+             "payment_followup_report.xml"
+        ],
+    'demo_xml': [
+    ],
+    'test':[
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application':False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ nsm_followup_layout/res_partner.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 BAS Solutions
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import osv
+
+
+class res_partner(osv.osv):
+
+    _inherit = "res.partner"
+    
+    def do_partner_print(self, cr, uid, wizard_partner_ids, data, context=None):
+        #wizard_partner_ids are ids from special view, not from res.partner
+        if not wizard_partner_ids:
+            return {}
+        data['partner_ids'] = wizard_partner_ids
+        datas = {
+             'ids': [],
+             'model': 'account_followup.followup',
+             'form': data
+        }
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'account_followup.followup.print.nsm',
+            'datas': datas,
+        }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
