337ab07cb0bc894aad3f0b5bc0c0940a05ea7e1e
Krupesh Laiya <krupesh@bizzappdev.com>
2014-10-30 16:52:20 +0530

    Add basic file for supplier portal and create new menu Supplier

---
+++ nsm_supplier_portal/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 BAS Solutions
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Supplier Portal',
+    'version': '1.0',
+    'category': 'Invoice',
+    'summary': 'Invoice',
+    'description': """This module holds the BAS-ERP Portal enhanchements.
+    """,
+    'author': 'BAS Solutions',
+    'website': 'http://www.bas-solutions.nl',
+    'depends': ['base', 'account',],
+    'data': [
+        "security/supplier_portal_security_view.xml",
+        "security/ir.model.access.csv",
+        "menu_view.xml",
+        "account_invoice_view.xml",
+        "product_view.xml",
+    ],
+    'demo': [],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
