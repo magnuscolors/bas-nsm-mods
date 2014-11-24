ffdcb810efbfb9f6b1d7cf250c66353f7dfbb403
Krupesh Laiya <krupesh@bizzappdev.com>
2014-11-24 14:27:08 +0530

    Remove comment from openerp file

--- nsm_supplier_portal/__openerp__.py
+++ nsm_supplier_portal/__openerp__.py
@@ -27,8 +27,7 @@
     """,
     'author': 'BAS Solutions',
     'website': 'http://www.bas-solutions.nl',
-#    'depends': ['base', 'account', 'mail', 'account_analytic_analysis', 'portal', 'sale_crm', 'megis_auth', 'megis_improv', 'web_m2x_options', 'auth_signup'],
-    'depends': ['base', 'account', 'mail', 'account_analytic_analysis', 'portal', 'sale_crm', 'web_m2x_options', 'auth_signup'],
+    'depends': ['base', 'account', 'mail', 'account_analytic_analysis', 'portal', 'sale_crm', 'megis_auth', 'megis_improv', 'web_m2x_options', 'auth_signup'],
     'data': [
         "data/crm_sale_team_data.xml",
         "data/auth_signup_send_mail_data.xml",
