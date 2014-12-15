abf98467e5334bf75890adb7635f5c67305d5b45
Ruchir Shukla <ruchir@bizzappdev.com>
2014-12-15 12:53:15 +0530

    add dependancy for the stock T00113

--- nsm_supplier_portal/__openerp__.py
+++ nsm_supplier_portal/__openerp__.py
@@ -28,7 +28,7 @@
     """,
     'author': 'BAS Solutions',
     'website': 'https://www.bas-solutions.nl',
-    'depends': ['base', 'account', 'mail', 'account_analytic_analysis', 'portal', 'sale_crm', 'megis_auth', 'megis_improv', 'web_m2x_options', 'portal'],
+    'depends': ['base', 'account', 'mail', 'account_analytic_analysis', 'portal', 'sale_crm', 'megis_auth', 'megis_improv', 'web_m2x_options', 'portal', "stock"],
     'data': [
         "data/crm_sale_team_data.xml",
         "data/auth_signup_send_mail_data.xml",
