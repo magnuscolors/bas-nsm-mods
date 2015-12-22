551be34ade690ddf5322a2e66a0b2d82c8fa73ee
Erwin van der Ploeg <erwin@bas-solutions.nl>
2015-12-22 15:45:43 +0100

    2 new modules added

---
+++ nsm_misc_customisations/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2015 Odoo Experts
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'NSM Misc Customisation',
+    'version': '1.0.8',
+    'category': 'account',
+    'sequence': 14,
+    'summary': 'NSM Misc Customisation',
+    'description': """
+        NSM Misc Customisation
+    """,
+    'author': 'Odoo Experts',
+    'website': 'https://www.odooexperts.nl',
+    'depends': ['base', 'account'],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'js': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
